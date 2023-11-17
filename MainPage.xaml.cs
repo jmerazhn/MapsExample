66ce50cab8d674ce527977179e3387ebf3d63f14
Josue Meraz <josuemeraz7@gmail.com>
2023-11-17 15:24:33 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using Map = Microsoft.Maui.Controls.Maps.Map;
+namespace MapsExample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Map map = new Map();
+            Content = map;
+        }
+
+    }
+}
